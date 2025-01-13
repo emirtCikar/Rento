8b7c2f98a5305e202d605f08d151521a85b3d071
emirtCikar <emir.t.cikar@gmail.com>
2025-01-13 06:41:57 +0300

    update

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Assignment2.Data;
+using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using static Assignment2.Models.Car;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Assignment2Context db = new Assignment2Context();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Car_List()
+        {
+            var cars = db.Cars.ToList();
+            return View(cars);
+           
+        }
+
+        public ActionResult Car_Info(int? id)
+        {
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Car car = db.Cars.Find(id);
+                if (car == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(car);
+            }
+        }
+
+
+    }
+
+}
---
+++ Models/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Assignment2.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+
+        public String Maker { get; set; }
+
+        public String Model { get; set; }
+
+        public int Year { get; set; }
+
+        public Type CType { get; set; }
+
+        public String CImage { get; set; }
+
+        public float Price { get; set; }
+
+        public bool CAvailable { get; set; }
+
+    }
+    public enum Type
+    {
+        Normal,
+        Family,
+        Sport
+    }
+}
